83df13a11958bbd495956b76aa8e73be9b7127f0
NartRamadan <kataz91@gmail.com>
2025-07-26 06:08:33 +0300

    Update client.ino

--- Final_IOT_EscapeRoom/Clinet/client.ino
+++ Final_IOT_EscapeRoom/Clinet/client.ino
@@ -1,6 +1,10 @@
 #include "config.h"
 #include "game_logic.h"
 
+// משתנים לניהול התאורה הראשונית
+bool roomLightOn = false; // האם האור בחדר דולק
+bool doorLocked = false; // האם הדלת נעולה
+
 void setup() {
     Serial.begin(9600); // אתחול תקשורת סריאלית
 
@@ -24,35 +28,66 @@ void setup() {
         Serial.println("Connecting to WiFi...");
     }
     Serial.println("Connected to WiFi");
+    
+    // אתחול גנרטור מספרים אקראיים
+    randomSeed(analogRead(A0));
+    
+    // אתחול מצב התאורה - כבוי בהתחלה
+    analogWrite(LIGHT_PIN, 0);
+    Serial.println("Escape Room Ready! Turn on the light to start...");
 }
 
 void loop() {
-    switch (currentStage) {
-    case 0: 
-        if (digitalRead(BUTTON_RED_PIN) == LOW) { 
-            solveChallenge1(); // פותר חידה 1
-        }
-        break;
+    // בדיקה אם האור הודלק בפעם הראשונה (בדיקה של חיישן אור או כפתור)
+    if (!roomLightOn && digitalRead(BUTTON_RED_PIN) == LOW) {
+        roomLightOn = true;
+        doorLocked = true;
+        Serial.println("Light turned on! Door is now locked. Solve the challenges to escape!");
+        
+        // הדלקת האור בעוצמה מלאה
+        analogWrite(LIGHT_PIN, 255);
+        delay(1000); // מניעת ריבאונד
+    }
+    
+    // רק אם הדלת נעולה, מתחיל את החידות
+    if (doorLocked) {
+        switch (currentStage) {
+        case 0: 
+            // חידה 1 - שליטה על התאורה
+            if (digitalRead(BUTTON_GREEN_PIN) == LOW) { 
+                solveChallenge1(); // פותר חידה 1
+            }
+            break;
 
-    case 1:
-        if (analogRead(TEMP_SENSOR_PIN) > 500) { 
+        case 1:
+            // חידה 2 - בקרת טמפרטורה
             solveChallenge2(); // פותר חידה 2
+            break;
+
+        case 2:
+            // חידה 3 - רצף הבהובים
+            solveChallenge3(); // מטפל בחידה 3
+            
+            // בדיקת קלט מהמשתמש (בחידה 3)
+            if (digitalRead(BUTTON_RED_PIN) == LOW) {
+                handleUserInput(LED_RED);
+                delay(200); // מניעת ריבאונד
+            }
+            if (digitalRead(BUTTON_GREEN_PIN) == LOW) {
+                handleUserInput(LED_GREEN);
+                delay(200); // מניעת ריבאונד
+            }
+            break;
+
+        case 3:
+            // חידה 4 - מדידת מרחק
+            solveChallenge4(); // פותר חידה 4
+            break;
+
+        default:
+            Serial.println("All challenges solved! Enter the code on the server to unlock the door!");
+            break;
         }
-        break;
-
-    case 2:
-        if (digitalRead(BUTTON_GREEN_PIN) == LOW) { 
-            solveChallenge3(); // פותר חידה 3
-        }
-        break;
-
-    case 3:
-        solveChallenge4(); // פותר חידה 4
-        break;
-
-    default:
-        Serial.println("All challenges solved!"); // כל החידות נפתרו
-        break;
     }
 
     delay(100); // מונע עבודה מאומצת מדי של הלולאה
